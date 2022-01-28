85a58ca544532d87a28300950ce21b05cd205662
megamendhie <imendhie@yahoo.com>
2022-01-28 13:02:14 +0100

    configured room db

--- app/src/main/java/com/mendhie/weatherapp/data/database/AppDatabase.kt
+++ app/src/main/java/com/mendhie/weatherapp/data/database/AppDatabase.kt
@@ -1,8 +1,21 @@
 package com.mendhie.weatherapp.data.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.mendhie.weatherapp.data.models.WeatherConverter
 import com.mendhie.weatherapp.data.models.WeatherForecast
+import javax.inject.Inject
 
-//@Database(entities = [WeatherForecast::class], version = 1)
-class AppDatabase {
+@Database(entities = [WeatherForecast::class], version = 1)
+@TypeConverters(WeatherConverter::class)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun WeatherDao(): WeatherDao
+
+    class WeatherCallback @Inject constructor(): RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+        }
+    }
 }
--- app/src/main/java/com/mendhie/weatherapp/data/database/WeatherDao.kt
+++ app/src/main/java/com/mendhie/weatherapp/data/database/WeatherDao.kt
@@ -1,4 +1,19 @@
 package com.mendhie.weatherapp.data.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.mendhie.weatherapp.data.models.WeatherConverter
+import com.mendhie.weatherapp.data.models.WeatherForecast
+
+@Dao
 interface WeatherDao {
+
+    @Query("SELECT * FROM weather_table")
+    fun getWeatherForecast(): LiveData<WeatherForecast>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(weatherForecast: WeatherForecast)
+
+    @Query("DELETE FROM weather_table")
+    suspend fun deleteAll()
 }
